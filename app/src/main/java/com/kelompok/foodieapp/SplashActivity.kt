b1acf2a74a30973f014c9adb2185521f4cf9fb68
Muhammad Azzami Yahya <4zzyhy@gmail.com>
2026-04-25 11:00:43 +0700

    feat: menambahkan tampilan splash, tampilan login, variabel string untuk placeholder form login, dan font baru

---
+++ app/src/main/java/com/kelompok/foodieapp/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.kelompok.foodieapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        // Pindah ke Login setelah 2 detik
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
