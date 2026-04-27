2ede9d934e5e40e5fe99b8ea9d35425196cbd2ad
Fadlan Fadilah <fadlanfadilah250@gmail.com>
2026-04-27 19:11:17 +0700

    feat: Menambahkan logo aplikasi dan tombol back pada halaman katalog

--- app/src/main/java/com/kelompok/foodieapp/MainActivity.kt
+++ app/src/main/java/com/kelompok/foodieapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.kelompok.foodieapp.databinding.ActivityMainBinding
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
